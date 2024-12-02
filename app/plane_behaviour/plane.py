296704eff9f5e946c4b74fc524e451b1096e4d40
s2230095 <s2230095@ed.ac.uk>
2024-12-02 11:22:31 +0000

    more commenting.

--- app/plane_behaviour/plane.py
+++ app/plane_behaviour/plane.py
@@ -1,7 +1,7 @@
 import numpy as np
 from utils import get_cell, set_cell
 import sys
-sys.tracebacklimit = 0
+sys.tracebacklimit = 0          # stop certain deprecation warnings from clogging up the terminal.
 sys.path.append('./app/plane_behaviour/')
 from clustering import find_fire_clusters
 
