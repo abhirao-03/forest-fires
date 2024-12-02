5fb72bdb973bab6d793e3a531b5a681271238fff
s2230095 <s2230095@ed.ac.uk>
2024-12-02 11:21:51 +0000

    added comments to clustering algorithm

--- app/plane_behaviour/clustering.py
+++ app/plane_behaviour/clustering.py
@@ -16,42 +16,45 @@ def find_fire_clusters(in_grid):
     grid[grid != 3] = int(0)
     grid[grid == 3] = int(1)
     
-
     rows, cols = len(grid), len(grid[0])
 
-    visited = [[False] * cols for _ in range(rows)]
+    visited = [[False] * cols for _ in range(rows)]         # generate an array of True/False values to track visited/non-visted cells.
 
     clusters = {}               # store clusters in a dictionary containing the size and the cells that are contained in the cluster.
     cluster_id = 1              # start labelling clusters with id 1
 
     for i in range(rows):
         for j in range(cols):
-            if grid[i][j] == 1 and not visited[i][j]:  # New unvisited fire cell found
-                # Initialize a new cluster
+            if grid[i][j] == 1 and not visited[i][j]:  # new unvisited fire cell found
+                
+                # initialize a new cluster
                 cluster_size = 0
                 cluster_cells = []
-                stack = [(i, j)]  # Start DFS from this cell
+                stack = [(i, j)]  # start DFS from this cell
                 
                 # Perform DFS to find all connected cells in this cluster
                 while stack:
-                    x, y = stack.pop()
+                    x, y = stack.pop()                  # take the last element from the appended list and continue.
 
                     if visited[x][y]:
                         continue
 
-                    visited[x][y] = True
+                    visited[x][y] = True                # since we've visited this cell set it to True.
+                    
+                    # add this cell to our clusters dictionary and add 1 to the cluster_size.
                     cluster_size += 1
                     cluster_cells.append((x, y))
-                    grid[x][y] = cluster_id  # Mark cell with the current cluster ID
+                    grid[x][y] = cluster_id             # mark cell with the current cluster ID
                     
-                    # Check all neighbors
+                    # check all neighbors using the hexagonal neighbouring
                     for dx, dy in effective_radius:
                         nx, ny = x + dx, y + dy
 
+                        # make sure we're within the bounds of the grid.
                         if 0 <= nx < rows and 0 <= ny < cols and not visited[nx][ny] and grid[nx][ny] == 1:
                             stack.append((nx, ny))
                 
-                # Store cluster details
+                # store cluster details in the dictionary
                 clusters[cluster_id] = {'size': cluster_size, 'cells': cluster_cells}
                 cluster_id += 1
 
